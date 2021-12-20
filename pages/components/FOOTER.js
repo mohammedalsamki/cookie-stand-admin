ec9c14a062174a4323342f3ee352d51f40fa8d25
mohammedalsamki <momosamak21@gmail.com>
2021-12-20 12:52:04 +0200

    solved lab37

---
+++ pages/components/FOOTER.js
@@ -0,0 +1,12 @@
+const FOOTER = () => {
+  return (
+    <footer
+      className='p-4'
+      style={{ backgroundColor: '#15B981' }}
+    >
+      &copy;2021 - mohammed alsamki
+    </footer>
+  )
+}
+
+export default FOOTER
